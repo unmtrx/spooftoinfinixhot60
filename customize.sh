a838769a99763c8b9b6742f477c286e3cb6d7379
calvinnoviano <105774329+unmtrx@users.noreply.github.com>
2026-02-26 11:27:06 +0700

    Add files via upload

---
+++ customize.sh
@@ -0,0 +1,7 @@
+ui_print "S24Ultra Spoofer"
+ui_print "Spoof Your Device To S24 Ultra Spoofer"
+ui_print "v7.1.0"
+ui_print "By: MRX7014"
+ui_print "Don't forget to join MRX7014 Cloud!"
+sleep 3
+am start -a android.intent.action.VIEW -d "https://t.me/mrx7014cloud"
